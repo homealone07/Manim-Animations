42aa63d2697e01b21f9a9d1084711f7d724a45e9
VenkatAmith <98298826+homealone07@users.noreply.github.com>
2022-02-23 11:19:04 +0530

    Update and rename Intro.py to GroupIntro.py

--- GroupIntro.py
+++ GroupIntro.py
@@ -1,3 +1,4 @@
+#First Code
 from manim import *
 
 class UnwriteReverseFalse(Scene):
@@ -114,3 +115,64 @@ class UnwriteReverseFalse(Scene):
             ),
             run_time=1
         )
+
+        
+#2nd Code
+
+from manim import *
+
+class UnwriteReverseFalse(Scene):
+    def construct(self):
+        text = Tex("Hello we are team").scale(3)
+        self.add(text)
+        self.wait(0.5)
+        self.play(Unwrite(text, reverse=False))
+        r = Tex("H","E","L","I","X").scale(2.5)
+        r[0].set_color(RED)
+        r[1].set_color(BLUE)
+        r[2].set_color(YELLOW)
+        r[3].set_color(GREEN)
+        r[4].set_color(ORANGE)
+        self.play(Write(r),run_time=3)
+        self.play(Unwrite(r, reverse=False))
+        self.play(Unwrite(text, reverse=False))
+        text = Tex("Abhay Arora" , color = RED).scale(2)
+        reg = Tex("20BRS1224" , color = BLUE).scale(2)
+        self.add(text)
+        self.wait(0.6)
+        self.play(Unwrite(text))
+        self.add(reg)
+        self.wait(0.6)
+        self.play(Unwrite(reg))
+        text = Tex("Venkat Amith Woonna" , color = RED).scale(2)
+        reg = Tex("20BCE1222" , color = BLUE).scale(2)
+        self.add(text)
+        self.wait(0.6)
+        self.play(Unwrite(text))
+        self.add(reg)
+        self.wait(0.6)
+        self.play(Unwrite(reg))
+        text = Tex("Gaurav Jena" , color = RED).scale(2)
+        reg = Tex("20BCE1233" , color = BLUE).scale(2)
+        self.add(text)
+        self.wait(0.6)
+        self.play(Unwrite(text))
+        self.add(reg)
+        self.wait(0.6)
+        self.play(Unwrite(reg))
+        text = Tex("Sai Kailash" , color = RED).scale(2)
+        reg = Tex("20BRS1208" , color = BLUE).scale(2)
+        self.add(text)
+        self.wait(0.6)
+        self.play(Unwrite(text))
+        self.add(reg)
+        self.wait(0.6)
+        self.play(Unwrite(reg))
+        text = Tex("Vamsi Vikkurty" , color = RED).scale(2)
+        reg = Tex("20BCE1066" , color = BLUE).scale(2)
+        self.add(text)
+        self.wait(0.6)
+        self.play(Unwrite(text))
+        self.add(reg)
+        self.wait(0.6)
+        self.play(Unwrite(reg, reverse=False))
