42b5ec57f9ee286737f2990598476a1a75d7b876
VenkatAmith <98298826+homealone07@users.noreply.github.com>
2022-03-15 22:45:00 +0530

    Update WeAreGroup.py

--- WeAreGroup.py
+++ WeAreGroup.py
@@ -6,9 +6,9 @@ def construct(self):
      b.set_color_by_tex("Abhay",RED)
      b.set_color_by_tex("Aditi",GREEN)
      b.set_color_by_tex("Amogh",BLUE)
-     b.set_color_by_tex("Abhay",BLUE)
-     b.set_color_by_tex("Aditi",GREEN)
-     b.set_color_by_tex("Amogh",RED)
+     b.set_color_by_tex("Amith",BLUE)
+     b.set_color_by_tex("Sarath",GREEN)
+     b.set_color_by_tex("Pradhyuman",RED)
 
      self.play(Write(t))
      self.play(Write(b),run_time=6)
