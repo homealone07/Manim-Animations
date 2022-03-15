7a3c427d56745d29d8c6f3d3ceb8f13e2a1bfb51
VenkatAmith <98298826+homealone07@users.noreply.github.com>
2022-03-15 21:57:53 +0530

    Update BlinkingFont.py

--- BlinkingFont.py
+++ BlinkingFont.py
@@ -8,3 +8,13 @@ class Test(Scene):
         self.play(ReplacementTransform(text, transform))
         self.wait()
         self.play(FadeOut(transform, shift=UP))
+
+        
+#2nd CODE
+#https://discord.com/channels/581738731934056449/780234936980078602/951122704398950450
+from manim import *
+class Test(Scene):
+    def construct(self):
+        text = Text("Hello", font_size=144, color=RED)
+        self.play(Create(text))
+        self.play(FadeOut(text, shift=UP))
